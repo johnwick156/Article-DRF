f3a9330eb9f64ad94dd76914fd5c62705f2b6d57
karthik <karthikj156@gmail.com>
2020-08-11 18:55:09 +0530

    Created serializers

--- article/views.py
+++ article/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+from .models import Article
+from .serializers import ArticleSerializer
