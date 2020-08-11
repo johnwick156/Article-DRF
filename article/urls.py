c0472311d5e3a2ddd8f08931149c310e92a5c652
karthik <karthikj156@gmail.com>
2020-08-11 20:13:21 +0530

    Serializer, JSON response, function based views

---
+++ article/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.article_list, name="list"),
+    path('detail/<int:pk>/', views.article_detail, name="detail"),
+]
--- article/views.py
+++ article/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
 from .models import Article
 from .serializers import ArticleSerializer
+
+@csrf_exempt
+def article_list(request):
+    
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+        return JsonResponse(serializer.data, status=201)
+
+@csrf_exempt
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        article.delete()
